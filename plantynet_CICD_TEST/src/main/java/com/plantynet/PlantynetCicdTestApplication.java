67f75fa4c75a548524a022a27f8bcfe156806e57
황병철 <bottleiron@plantynet.com>
2024-08-22 14:33:01 +0900

    1st commit

---
+++ plantynet_CICD_TEST/src/main/java/com/plantynet/PlantynetCicdTestApplication.java
@@ -0,0 +1,13 @@
+package com.plantynet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantynetCicdTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantynetCicdTestApplication.class, args);
+	}
+
+}
---
+++ plantynet_CICD_TEST/src/test/java/com/plantynet/PlantynetCicdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantynet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantynetCicdTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
